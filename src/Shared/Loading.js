a727552b10f4ca7610356c887f01b4fcb712c76a
amir047123 <amirfaysal047123@gmail.com>
2024-03-13 14:20:09 +0600

    done

--- src/Shared/Loading.js
+++ src/Shared/Loading.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { DNA } from "react-loader-spinner";
+import styled, { keyframes } from "styled-components";
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
 
 const Loading = () => {
+  const CustomLoader = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  `;
+
+  const Loader = styled.div`
+    border: 8px solid #f3f3f3; /* Light grey */
+    border-top: 8px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 80px;
+    height: 80px;
+    animation: ${spin} 2s linear infinite;
+  `;
+
   return (
-    <div className="h-screen flex justify-center items-center w-full">
-      <DNA
-        color="#00BFFF"
-        height={80}
-        width={80}
-        ariaLabel="dna-loading"
-        className="dna-wrapper"
-      />
-    </div>
+    <CustomLoader>
+      <Loader />
+    </CustomLoader>
   );
 };
 
