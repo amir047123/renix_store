f4ca01e970bdc554d10c327e75420e0bf3e87bbf
Reshma62 <mernstack2024@gmail.com>
2024-03-17 14:45:17 +0600

    add

--- src/components/DataLayer/DataLayer.js
+++ src/components/DataLayer/DataLayer.js
@@ -1,10 +1,12 @@
-// DataLayer.js
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const DataLayer = ({ data }) => {
   useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push(data);
+    // Check if the data layer is empty before pushing the data
+    if (window.dataLayer && window.dataLayer.length === 0) {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push(data);
+    }
   }, [data]);
 
   return null;
